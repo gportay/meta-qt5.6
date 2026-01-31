dilshodm/meta-qt5

Recipes: add explicit branch parameter and use https:// instead of git:// for github.com
* github decided that nobody should use git://
  https://github.blog/2021-09-01-improving-git-protocol-security-github/#no-more-unauthenticated-git

  and starting today all the builds were failing to fetch the metadata
  layers from github like:

  2021-11-01T18:53:26 INFO _main_ Updating [meta-ros]
  fatal: remote error:
    The unauthenticated git protocol on port 9418 is no longer supported.
  Please see https://github.blog/2021-09-01-improving-git-protocol-security-github/ for more information.

  this was just a "test" as they said:
  "November 2, 2021: We'll also run several short brownouts on this date."
  and it will be completely disabled on January 11 2022.

Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-qt/examples/qt5everywheredemo_1.0.bb
@@ -8,7 +8,8 @@ DEPENDS = "qtdeclarative qtgraphicaleffects qtsvg qtmultimedia qtxmlpatterns"
 
 SRCREV = "35d72a2eba7456a2efc5eb8b77afbc00f69ba0ac"
 QT_GIT_PROJECT = "qt-labs"
-SRC_URI = "${QT_GIT}/qt5-everywhere-demo"
+QT_MODULE_BRANCH_PARAM = "master"
+SRC_URI = "${QT_GIT}/qt5-everywhere-demo;branch=${QT_MODULE_BRANCH};protocol=${QT_GIT_PROTOCOL}"
 
 S = "${WORKDIR}/git/QtDemo"
 

gportay/meta-qt5.6

Signed-off-by: Gaël PORTAY <gael.portay@gmail.com>

recipes-qt/examples/qt5everywheredemo_1.0.bb
@@ -8,7 +8,8 @@ DEPENDS = "qtdeclarative qtgraphicaleffects"
 
 SRCREV = "c17fe9e0ec0882ac4c4dc1168095f569acab5d09"
 QT_GIT_PROJECT = "qt-labs"
-SRC_URI = "${QT_GIT}/qt5-everywhere-demo"
+QT_MODULE_BRANCH = "master"
+SRC_URI = "${QT_GIT}/qt5-everywhere-demo;branch=${QT_MODULE_BRANCH};protocol=${QT_GIT_PROTOCOL}"
 
 S = "${WORKDIR}/git/QtDemo"
 